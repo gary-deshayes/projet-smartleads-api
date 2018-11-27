2388228287f97ad5618fa97e83b8e981bce94088
Vigni Lucas <lucas.vignijr@gmail.com>
2018-11-27 15:02:17 +0100

    Entities vigni

---
+++ src/Entity/Company.php
@@ -0,0 +1,380 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CompanyRepository")
+ */
+class Company
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $company_code;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at_plug;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $update_at_plug;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $leader_code;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $logo;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $commentary;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $coutry;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $adress;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $adress_complement;
+
+    /**
+     * @ORM\Column(type="string", length=5, nullable=true)
+     */
+    private $postal_code;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private $fax;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $website;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $adress_commentary;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $siret_number;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $naf_code;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $source;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCompanyCode(): ?string
+    {
+        return $this->company_code;
+    }
+
+    public function setCompanyCode(string $company_code): self
+    {
+        $this->company_code = $company_code;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAtPlug(): ?\DateTimeInterface
+    {
+        return $this->created_at_plug;
+    }
+
+    public function setCreatedAtPlug(\DateTimeInterface $created_at_plug): self
+    {
+        $this->created_at_plug = $created_at_plug;
+
+        return $this;
+    }
+
+    public function getUpdateAtPlug(): ?\DateTimeInterface
+    {
+        return $this->update_at_plug;
+    }
+
+    public function setUpdateAtPlug(\DateTimeInterface $update_at_plug): self
+    {
+        $this->update_at_plug = $update_at_plug;
+
+        return $this;
+    }
+
+    public function getLeaderCode(): ?string
+    {
+        return $this->leader_code;
+    }
+
+    public function setLeaderCode(?string $leader_code): self
+    {
+        $this->leader_code = $leader_code;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(?string $logo): self
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+
+    public function getCommentary(): ?string
+    {
+        return $this->commentary;
+    }
+
+    public function setCommentary(?string $commentary): self
+    {
+        $this->commentary = $commentary;
+
+        return $this;
+    }
+
+    public function getCoutry(): ?string
+    {
+        return $this->coutry;
+    }
+
+    public function setCoutry(?string $coutry): self
+    {
+        $this->coutry = $coutry;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(?string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getAdressComplement(): ?string
+    {
+        return $this->adress_complement;
+    }
+
+    public function setAdressComplement(?string $adress_complement): self
+    {
+        $this->adress_complement = $adress_complement;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postal_code;
+    }
+
+    public function setPostalCode(?string $postal_code): self
+    {
+        $this->postal_code = $postal_code;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getFax(): ?string
+    {
+        return $this->fax;
+    }
+
+    public function setFax(?string $fax): self
+    {
+        $this->fax = $fax;
+
+        return $this;
+    }
+
+    public function getWebsite(): ?string
+    {
+        return $this->website;
+    }
+
+    public function setWebsite(?string $website): self
+    {
+        $this->website = $website;
+
+        return $this;
+    }
+
+    public function getAdressCommentary(): ?string
+    {
+        return $this->adress_commentary;
+    }
+
+    public function setAdressCommentary(?string $adress_commentary): self
+    {
+        $this->adress_commentary = $adress_commentary;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getSiretNumber(): ?string
+    {
+        return $this->siret_number;
+    }
+
+    public function setSiretNumber(string $siret_number): self
+    {
+        $this->siret_number = $siret_number;
+
+        return $this;
+    }
+
+    public function getNafCode(): ?string
+    {
+        return $this->naf_code;
+    }
+
+    public function setNafCode(?string $naf_code): self
+    {
+        $this->naf_code = $naf_code;
+
+        return $this;
+    }
+
+    public function getSource(): ?string
+    {
+        return $this->source;
+    }
+
+    public function setSource(?string $source): self
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Contact.php
@@ -0,0 +1,414 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $code_customer;
+
+    /**
+     * @ORM\Column(type="string", length=1)
+     */
+    private $gender;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_created_plug;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_update_plug;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $decision_level;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $birth_date;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $job;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private $mobile_phone;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $email_prechecked;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $email_checked;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $url_linkedin;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $photo;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $operation_source;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $commentary;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $opt_in_newsletter;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $opt_in_offer_commercial;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $company_function;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $company_service;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCodeCustomer(): ?string
+    {
+        return $this->code_customer;
+    }
+
+    public function setCodeCustomer(string $code_customer): self
+    {
+        $this->code_customer = $code_customer;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDateCreatedPlug(): ?\DateTimeInterface
+    {
+        return $this->date_created_plug;
+    }
+
+    public function setDateCreatedPlug(\DateTimeInterface $date_created_plug): self
+    {
+        $this->date_created_plug = $date_created_plug;
+
+        return $this;
+    }
+
+    public function getDateUpdatePlug(): ?\DateTimeInterface
+    {
+        return $this->date_update_plug;
+    }
+
+    public function setDateUpdatePlug(\DateTimeInterface $date_update_plug): self
+    {
+        $this->date_update_plug = $date_update_plug;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getDecisionLevel(): ?int
+    {
+        return $this->decision_level;
+    }
+
+    public function setDecisionLevel(?int $decision_level): self
+    {
+        $this->decision_level = $decision_level;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeInterface
+    {
+        return $this->birth_date;
+    }
+
+    public function setBirthDate(?\DateTimeInterface $birth_date): self
+    {
+        $this->birth_date = $birth_date;
+
+        return $this;
+    }
+
+    public function getJob(): ?string
+    {
+        return $this->job;
+    }
+
+    public function setJob(?string $job): self
+    {
+        $this->job = $job;
+
+        return $this;
+    }
+
+    public function getMobilePhone(): ?string
+    {
+        return $this->mobile_phone;
+    }
+
+    public function setMobilePhone(?string $mobile_phone): self
+    {
+        $this->mobile_phone = $mobile_phone;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getEmailPrechecked(): ?bool
+    {
+        return $this->email_prechecked;
+    }
+
+    public function setEmailPrechecked(?bool $email_prechecked): self
+    {
+        $this->email_prechecked = $email_prechecked;
+
+        return $this;
+    }
+
+    public function getEmailChecked(): ?bool
+    {
+        return $this->email_checked;
+    }
+
+    public function setEmailChecked(?bool $email_checked): self
+    {
+        $this->email_checked = $email_checked;
+
+        return $this;
+    }
+
+    public function getUrlLinkedin(): ?string
+    {
+        return $this->url_linkedin;
+    }
+
+    public function setUrlLinkedin(?string $url_linkedin): self
+    {
+        $this->url_linkedin = $url_linkedin;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getOperationSource(): ?string
+    {
+        return $this->operation_source;
+    }
+
+    public function setOperationSource(?string $operation_source): self
+    {
+        $this->operation_source = $operation_source;
+
+        return $this;
+    }
+
+    public function getCommentary(): ?string
+    {
+        return $this->commentary;
+    }
+
+    public function setCommentary(?string $commentary): self
+    {
+        $this->commentary = $commentary;
+
+        return $this;
+    }
+
+    public function getOptInNewsletter(): ?bool
+    {
+        return $this->opt_in_newsletter;
+    }
+
+    public function setOptInNewsletter(bool $opt_in_newsletter): self
+    {
+        $this->opt_in_newsletter = $opt_in_newsletter;
+
+        return $this;
+    }
+
+    public function getOptInOfferCommercial(): ?bool
+    {
+        return $this->opt_in_offer_commercial;
+    }
+
+    public function setOptInOfferCommercial(bool $opt_in_offer_commercial): self
+    {
+        $this->opt_in_offer_commercial = $opt_in_offer_commercial;
+
+        return $this;
+    }
+
+    public function getCompanyFunction(): ?string
+    {
+        return $this->company_function;
+    }
+
+    public function setCompanyFunction(string $company_function): self
+    {
+        $this->company_function = $company_function;
+
+        return $this;
+    }
+
+    public function getCompanyService(): ?string
+    {
+        return $this->company_service;
+    }
+
+    public function setCompanyService(string $company_service): self
+    {
+        $this->company_service = $company_service;
+
+        return $this;
+    }
+}
