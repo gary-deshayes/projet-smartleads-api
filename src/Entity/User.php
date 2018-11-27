f25c226820113848bceede36ca890f47686dea6d
Baptiste Rossignol <baptiste.rossignol@hotmail.fr>
2018-11-27 14:52:23 +0100

    Entities B

---
+++ src/Entity/User.php
@@ -0,0 +1,278 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=1)
+     */
+    private $gender;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $profil;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $update_at;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $statement;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $birth_date;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $job_name;
+
+    /**
+     * @ORM\Column(type="string", length=15, nullable=true)
+     */
+    private $tel_mobile;
+
+    /**
+     * @ORM\Column(type="string", length=15, nullable=true)
+     */
+    private $tel_fixe;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $url_linkedin;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $photo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getProfil(): ?string
+    {
+        return $this->profil;
+    }
+
+    public function setProfil(string $profil): self
+    {
+        $this->profil = $profil;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->update_at;
+    }
+
+    public function setUpdateAt(?\DateTimeInterface $update_at): self
+    {
+        $this->update_at = $update_at;
+
+        return $this;
+    }
+
+    public function getStatement(): ?bool
+    {
+        return $this->statement;
+    }
+
+    public function setStatement(?bool $statement): self
+    {
+        $this->statement = $statement;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeInterface
+    {
+        return $this->birth_date;
+    }
+
+    public function setBirthDate(?\DateTimeInterface $birth_date): self
+    {
+        $this->birth_date = $birth_date;
+
+        return $this;
+    }
+
+    public function getJobName(): ?string
+    {
+        return $this->job_name;
+    }
+
+    public function setJobName(?string $job_name): self
+    {
+        $this->job_name = $job_name;
+
+        return $this;
+    }
+
+    public function getTelMobile(): ?string
+    {
+        return $this->tel_mobile;
+    }
+
+    public function setTelMobile(?string $tel_mobile): self
+    {
+        $this->tel_mobile = $tel_mobile;
+
+        return $this;
+    }
+
+    public function getTelFixe(): ?string
+    {
+        return $this->tel_fixe;
+    }
+
+    public function setTelFixe(?string $tel_fixe): self
+    {
+        $this->tel_fixe = $tel_fixe;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUrlLinkedin(): ?string
+    {
+        return $this->url_linkedin;
+    }
+
+    public function setUrlLinkedin(?string $url_linkedin): self
+    {
+        $this->url_linkedin = $url_linkedin;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+}
---
+++ src/Repository/ParameterRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Parameter;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Parameter|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Parameter|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Parameter[]    findAll()
+ * @method Parameter[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ParameterRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Parameter::class);
+    }
+
+    // /**
+    //  * @return Parameter[] Returns an array of Parameter objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Parameter
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
