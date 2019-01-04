32ae52405ca1d53a1eeb9d9bc5633d5e5967ae05
Baptiste Rossignol <baptiste.rossignol@hotmail.fr>
2019-01-04 22:43:13 +0100

    Controller + Template + Form + Assert ParameterGraphicStyles

---
+++ src/Form/ParameterGraphicStylesType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParameterGraphicStylesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('label')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
