35da6752468dc46844c4a1e4ca39f0c014596981
VIGNI Lucas <vigni.pro@gmail.com>
2019-01-08 21:22:46 +0100

    resolve conflict use i ActivityArea1

--- src/Controller/CompanyActivityAreaController.php
+++ src/Controller/CompanyActivityAreaController.php
@@ -2,15 +2,7 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Entity\CompanyActivityArea;
-use App\Form\CompanyActivityAreaType;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\HttpFoundation\Response;
-use App\Repository\ParameterTargetRepository;
+
 
 /**
  * @Route("companyActivityArea")
