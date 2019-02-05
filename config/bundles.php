f42f8725b843f7ddbc2c765c4d1cce0fe55dffff
Maxime DUROYON <maxime.duroyonJR@gmail.com>
2019-02-05 08:58:50 +0100

    ajout du composer orm-fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     App\ApiBundle\ApiBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
