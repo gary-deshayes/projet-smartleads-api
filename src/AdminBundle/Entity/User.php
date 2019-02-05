ba45240336ecbec631a5c9e7e538a3774d9054fb
Baptiste Rossignol <baptiste.rossignol@hotmail.fr>
2019-02-05 10:25:17 +0100

    Entité USER

--- src/AdminBundle/Entity/User.php
+++ src/AdminBundle/Entity/User.php
@@ -31,7 +31,6 @@ class User
      */
     private $code;
 
-
     /**
      * @ORM\Column(type="string", length=100)
      * @Assert\NotBlank
@@ -172,6 +171,17 @@ class User
      * @ORM\ManyToOne(targetEntity="App\AdminBundle\Entity\Gender", inversedBy="users")
      */
     private $gender;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
     
   
 
@@ -543,5 +553,59 @@ class User
         return $this->name . " " . $this->first_name;
     }
 
+
+
+    // Ajout User Security 
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
    
 }
