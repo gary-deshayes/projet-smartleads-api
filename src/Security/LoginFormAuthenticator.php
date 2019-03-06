aaa9c7b5c40b8c5e9f9b551530e5da57831f21a7
Unknown <deshayesgary@hotmail.fr>
2019-03-06 14:01:22 +0100

    Mise en place Dashboard et redirection login

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -86,7 +86,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         // For example : 
-        return new RedirectResponse($this->urlGenerator->generate('company_index'));
+        return new RedirectResponse($this->urlGenerator->generate('dashboard'));
     }
 
     protected function getLoginUrl()
