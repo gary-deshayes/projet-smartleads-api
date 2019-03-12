29056de2a33c7ba00c078ee030b1e3e9815dd05a
Deshayes gary <gary.deshayesjr@gmail.com>
2019-03-12 23:50:38 +0100

    API get Contacts semaine non fini

--- src/ApiBundle/Controller/ContactsController.php
+++ src/ApiBundle/Controller/ContactsController.php
@@ -8,12 +8,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 /**
  * @Route("/contacts")
  */
 class ContactsController extends AbstractController
 {
+
+    /**
+     * Récupère le nombre de contacts pour chaque jour des 8 derniers jours passés
+     * @Route("/getlastweeknewcontacts", name="api_contacts_getlastweeknewcontacts", methods={"GET"})
+     */
+    public function lastWeekNewContacts(){
+        $contactsRepository = $this->getDoctrine()->getRepository("AdminBundle:Contacts");
+        $dateNow = date("Y-m-d");
+        $dateBefore = date("Y-m-d", strtotime('-7 days'));
+        $query = $contactsRepository->createQueryBuilder("contacts")
+             ->select("COUNT(contacts.createdAt) as nb, contacts.createdAt")
+             ->where("contacts.createdAt => :date_debut")
+             ->andWhere("contacts.createdAt <= :date_fin")
+             ->groupby("contacts.createdAt")
+             ->setParameter('date_debut', $dateBefore)
+             ->setParameter('date_fin', $dateNow)
+             ->getQuery();
+           dump($query->getResult());
+           SELECT COUNT(*), date(created_at) FROM contacts WHERE DATE(created_at) BETWEEN CURRENT_DATE()-8 AND CURRENT_DATE() GROUP BY DATE(created_at)
+
+
+
+                        
+        
+        
+        // $data = array("nombre" => count($companies));
+        // $response = new Response(json_encode($data), 200);
+        // $response->headers->set('Content-Type', 'application/json');
+        // return $response;
+
+    }
+
     /**
      * Récupération des contacts
      * @Route("/get/{id}", name="api_contacts_get", methods={"GET"})
