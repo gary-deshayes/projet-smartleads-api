d5f950413816f39631dbdf6442e2245873dbae8a
Vigni <lucas.vignijr@gmail.com>
2019-03-19 14:46:02 +0100

    Update form Sitting
    Update de l'édit de settings et ajout de checkbox sur commerciaux

--- src/AdminBundle/Entity/Settings.php
+++ src/AdminBundle/Entity/Settings.php
@@ -3,6 +3,7 @@
 namespace App\AdminBundle\Entity;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 use Doctrine\ORM\Mapping as ORM;
 
