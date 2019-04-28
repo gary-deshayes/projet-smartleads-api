c59ca600ae4c41e0776b3253c1bc18c492925999
Baptiste <baptiste.rossignol@hotmail.fr>
2019-04-28 14:27:49 +0200

    ADD: Donut component dashboard

--- public/js/application.js
+++ public/js/application.js
@@ -223,6 +223,42 @@ function changeStatutContact(value) {
 //     }
 // });
 
+var options = {
+    chart: {
+        type: 'donut',
+    },
+    dataLabels: {
+        enabled: true
+    },
+    plotOptions: {
+        pie: {
+            size: 100,
+            donut: {
+                size: '85%'
+            },
+
+        }
+      },
+    series: [44, 55, 41, 17, 15],
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            chart: {
+                width: 200
+            },
+            legend: {
+                position: 'bottom'
+            }
+        }
+    }]
+}
+
+var chart = new ApexCharts(
+    document.querySelector("#chart"),
+    options
+);
+
+chart.render();
 
 
 
