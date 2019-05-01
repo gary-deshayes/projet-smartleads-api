79aea9dbcf2ad1de274016f1a87debbf780723f3
Baptiste <baptiste.rossignol@hotmail.fr>
2019-05-01 15:24:23 +0200

    ADD: Component indicateurs perf

--- public/js/application.js
+++ public/js/application.js
@@ -317,10 +317,7 @@ var options = {
         size: 6
     },
     xaxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-        title: {
-            text: 'Month'
-        }
+        categories: ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jui', 'Juil']
     },
     legend: {
         position: 'top',
@@ -333,69 +330,6 @@ var options = {
 
 var chart = new ApexCharts(
     document.querySelector("#line-operation-chart"),
-    options
-);
-
-chart.render();
-
-var options = {
-    chart: {
-        height: 350,
-        type: 'line',
-        shadow: {
-            enabled: true,
-            color: '#000',
-            top: 18,
-            left: 7,
-            blur: 10,
-            opacity: 1
-        },
-        toolbar: {
-            show: false
-        }
-    },
-    colors: ['#77B6EA', '#545454'],
-    dataLabels: {
-        enabled: true,
-    },
-    series: [{
-            name: "High - 2013",
-            data: [28, 29, 33, 36, 32, 32, 33]
-        },
-        {
-            //Pour avoir le point en haut a droite
-            name: "",
-            data: []
-        }
-    ],
-    grid: {
-        borderColor: '#e7e7e7',
-        row: {
-            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-            opacity: 0.5
-        },
-    },
-    markers: {
-        
-        size: 6
-    },
-    xaxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-        title: {
-            text: 'Month'
-        }
-    },
-    legend: {
-        position: 'top',
-        horizontalAlign: 'right',
-        floating: true,
-        offsetY: -25,
-        offsetX: -5
-    }
-}
-
-var chart = new ApexCharts(
-    document.querySelector("#line-contact-new-chart"),
     options
 );
 
