3e6447db0dac9edb5aea95c262cd831144ff375b
Vigni <lucas.vignijr@gmail.com>
2019-05-01 17:54:32 +0200

    mot de passe oublié fonctionnel

--- src/AdminBundle/Controller/SecurityController.php
+++ src/AdminBundle/Controller/SecurityController.php
@@ -15,7 +15,8 @@ class SecurityController extends AbstractController
      * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils, ObjectManager $manager): Response
-    {
+    {   
+        $messReset = "";
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
